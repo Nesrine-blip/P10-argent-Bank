45c1e3499261b24a27a39caa13d03a99251f1874
Nesrine-blip <salahnesrine29@gmail.com>
2025-11-16 21:38:32 -0800

    edi server data

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
-const databaseUrl = 'mongodb://localhost:27017/argentBankDB'
+
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect('mongodb://localhost:27017/argentBankDB', { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true 
+    })
+    console.log('✅ Database successfully connected')
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
+    console.error(` Database Connectivity Error: ${error}`)
     throw new Error(error)
   }
 }
